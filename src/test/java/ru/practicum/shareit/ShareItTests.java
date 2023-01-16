3dfbaa8d8d686898fdc6e05eaaffdd4a02b6e2dc
Timur <akhmetzyanovtimur3@yandex.ru>
2023-01-16 13:43:37 +0500

    fix

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,16 +2,11 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest(classes = ShareItTests.class)
+@SpringBootTest
 class ShareItTests {
 
 	@Test
-	void contextLoads(ApplicationContext context) {
-		assertNotNull(context);
+	void contextLoads() {
 	}
-
 }
